5e36299947f3203608cca3c0b3c51bd61f8f6506
kar1sumax <szkkntru2@gmail.com>
2025-01-19 16:02:57 +0900

    5-2 processing

--- app/wtforms-sample/forms.py
+++ app/wtforms-sample/forms.py
@@ -5,24 +5,80 @@ from wtforms.fields import (
     EmailField, SubmitField
 )
 
+
 # ---------------------------
 # Formクラス
 # ユーザ情報クラス
+# class UserInfoForm(Form):
+# 	name = StringField("名前: ", render_kw={"placeholder": "例)山田 太郎"})
+# 	age = IntegerField("年齢: ", default=20)
+# 	password = PasswordField("パスワード: ")
+# 	confirm_password = PasswordField("パスワード確認: ")
+# 	email = EmailField("メールアドレス: ")
+# 	birthday = DateField("生年月日: ", format="%Y-%m-%d", render_kw={"placeholder": "yyyy/mm/dd"})
+# 	gender = RadioField(
+# 		"性別: ", choices=[("man", "男性"), ("woman", "女性")],
+# 		default = "man"
+# 	)
+# 	area = SelectField("出身地域: ", choices=[("east", "東日本"), ("west", "西日本")])
+# 	is_married = BooleanField("既婚？: ")
+# 	note = TextAreaField("備考: ")
+# 	submit = SubmitField("送信")
+
+
+
+# ---------------------------
+# バリデーションを追加する
+
+from wtforms.validators import (
+	DataRequired, EqualTo, Length, NumberRange, Email
+)
+
 class UserInfoForm(Form):
-	name = StringField("名前: ", render_kw={"placeholder": "例)山田 太郎"})
-	age = IntegerField("年齢: ", default=20)
-	password = PasswordField("パスワード: ")
+	name = StringField(
+		"名前: ",
+		validators=[DataRequired('名前は必須入力です')],
+		render_kw={"placeholder": "例)山田 太郎"}
+	)
+
+	age = IntegerField(
+		"年齢: ",
+		validators=[NumberRange(18, 100, '入力範囲は18歳から100歳です')],
+		default=20
+	)
+
+	password = PasswordField(
+		"パスワード: ",
+		validators=[Length(1, 10,
+                        'パスワードの長さは1文字以上10文字以内です'), 
+        EqualTo('confirm_password', 'パスワードが一致しません')]
+	)
+
 	confirm_password = PasswordField("パスワード確認: ")
-	email = EmailField("メールアドレス: ")
-	birthday = DateField("生年月日: ", format="%Y-%m-%d", render_kw={"placeholder": "yyyy/mm/dd"})
+	
+	email = EmailField(
+		"メールアドレス: ",
+		validators=[Email('メールアドレスのフォーマットではありません')]
+	)
+
+	birthday = DateField(
+		"生年月日: ",
+		validators=[DataRequired('生年月日は必須入力です')],
+		format="%Y-%m-%d",
+		render_kw={"placeholder": "yyyy/mm/dd"}
+	)
+	
 	gender = RadioField(
 		"性別: ", choices=[("man", "男性"), ("woman", "女性")],
 		default = "man"
 	)
+
 	area = SelectField("出身地域: ", choices=[("east", "東日本"), ("west", "西日本")])
 	is_married = BooleanField("既婚？: ")
 	note = TextAreaField("備考: ")
 	submit = SubmitField("送信")
+
+
 
 
 
--- for_presentation/chapter5/content.md
+++ for_presentation/chapter5/content.md
@@ -152,7 +152,7 @@ if __name__ == '__main__':
 ```
 
 
-```html;enter.html
+```html:enter.html
 {% extends "base.html" %}
 
 {% block title %}
@@ -188,13 +188,65 @@ if __name__ == '__main__':
 
 - ユーザが入力したデータが適切な形式であるかどうかを確認する方法である。例えば、メアドが正しい形式で入力されているかどうか、数字フィールドに数字が入力されているかどうか、などのチェックを行える。
 
-![バリデーション一覧](155012.png)
+![バリデーション一覧](imgs/155012.png)
+
+
+```python:forms.py
+# ---------------------------
+# バリデーションを追加する
+
+from wtforms.validators import (
+	DataRequired, EqualTo, Length, NumberRange, Email
+)
+
+class UserInfoForm(Form):
+	name = StringField(
+		"名前: ",
+		validators=[DataRequired('名前は必須入力です')],
+		render_kw={"placeholder": "例)山田 太郎"}
+	)
+
+	age = IntegerField(
+		"年齢: ",
+		validators=[NumberRange(18, 100, '入力範囲は18歳から100歳です')],
+		default=20
+	)
+
+	password = PasswordField(
+		"パスワード: ",
+		validators=[Length(1, 10,
+                        'パスワードの長さは1文字以上10文字以内です'), 
+        EqualTo('confirm_password', 'パスワードが一致しません')]
+	)
+
+	confirm_password = PasswordField("パスワード確認: ")
+	
+	email = EmailField(
+		"メールアドレス: ",
+		validators=[Email('メールアドレスのフォーマットではありません')]
+	)
+
+	birthday = DateField(
+		"生年月日: ",
+		validators=[DataRequired('生年月日は必須入力です')],
+		format="%Y-%m-%d",
+		render_kw={"placeholder": "yyyy/mm/dd"}
+	)
+	
+	gender = RadioField(
+		"性別: ", choices=[("man", "男性"), ("woman", "女性")],
+		default = "man"
+	)
+
+	area = SelectField("出身地域: ", choices=[("east", "東日本"), ("west", "西日本")])
+	is_married = BooleanField("既婚？: ")
+	note = TextAreaField("備考: ")
+	submit = SubmitField("送信")
+```
 
 
 
-
-
-
+- 実行すると、`Exception: Install 'email_validator' for email validation support.`のようなエラーが出る。その場合は、`pip install email-validator`しておく。
 
 
 
